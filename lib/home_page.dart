daf44e096bbdd186335cf6f7a981d75f014e910e
Anil Kandel <kandelanil24@gmail.com>
2024-02-25 21:08:43 +0545

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,91 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController _messageController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Message App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                  maxLength: 10,
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _messageController,
+                  // onChanged: (value) {
+                  //   log("Val: $value");
+                  //   setState(() {});
+                  // },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter a message";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: "Enter your message",
+                    labelStyle: TextStyle(color: Colors.blue),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  )),
+              const SizedBox(height: 15),
+              GestureDetector(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    log("Validate");
+                  } else {
+                    log("Not valid");
+                  }
+                  setState(() {});
+                  log(_messageController.text);
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: const Text(
+                    "Send Message",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text("Message: ${_messageController.text}")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
